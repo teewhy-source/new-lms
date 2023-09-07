2f28cee0497a07a6fd46f8e696b331c3a625bc7e
ayodelehallymah <ayodelehallymah@gmail.com>
2023-09-07 14:30:19 +0100

    uselcation

--- src/Components/Admin/SideBar/AdminSidebar.jsx
+++ src/Components/Admin/SideBar/AdminSidebar.jsx
@@ -7,8 +7,11 @@ import { GiBlackBook } from "react-icons/gi";
 import { MdApproval } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useLocation } from "react-router-dom";
 
 const AdminSideBar = () => {
+  const location = useLocation()
+  console.log(location.pathname);
   return (
     <div className="flex flex-col bg-[#5bbcb3] bg-opacity-30 shadow-lg shadow-green-700">
       <div className="font-bold mb-[2rem] md:flex w-[12rem]">
@@ -16,16 +19,16 @@ const AdminSideBar = () => {
         <h1 className="text-[#08538c] mt-[1.5rem] ml-[0.5rem] text-xl">LMS</h1>
       </div>
 
-      <div className="flex gap-2 bg-opacity-40 mb-[2.5rem] items-center pl-3">
+      <div className="flex gap-2 bg-opacity-40 mb-[2.5rem] items-center pl-3 ">
         <RiAdminLine className="h-[2.5rem] w-[2.5rem] text-[#14408b]" />
         <span className="font-bold text-[#14408b] text-[2rem] tracking-wider">
           ADMIN
         </span>
       </div>
 
-      <div className="flex mb-[1rem] items-center pl-3 w-[3rem] h-[2rem]">
+      <div className="flex mb-[1rem] items-center pl-3 w-[5rem] h-[2rem] flex-row">
         <Link to="/AdminDashBoard">
-          <h1 className="font-bold flex items-center justify-around gap-[0.3rem] cursor-pointer border-1 p-[0.4rem] text-[white]">
+          <h1 className= {`${location.pathname === "/AdminDashBoard" && "bg-teal-300"}`}>
             <RxDashboard /> Dashboard
           </h1>
         </Link>
@@ -33,7 +36,7 @@ const AdminSideBar = () => {
 
       <div className="flex gap-2 mb-[1rem] items-center pl-3 hover:bg-[#f2b907]">
         <Link to="/Instructor">
-          <h1 className="font-bold flex items-center justify-around gap-[0.3rem] cursor-pointer border-1 p-[0.4rem] text-[white]">
+          <h1 className= {`${location.pathname === "/Instructor" && "bg-teal-300 p-10"}`}>
             <RxAvatar className="h-[2rem] w-[1.5rem]" /> Instructor
           </h1>
         </Link>
@@ -41,25 +44,23 @@ const AdminSideBar = () => {
 
       <div className="flex gap-2 items-center mb-[1rem] pl-3 hover:bg-[#f2b907]">
         <Link to="/Coursepage">
-          <h1 className="font-bold flex items-center justify-around gap-[0.3rem] cursor-pointer border-1 p-[0.4rem] text-[white]">
+          <h1 className= {`${location.pathname === "/Coursepage" && "bg-teal-300 p-10"}`}>
             <GiBlackBook className="h-[2rem] w-[1.5rem]" /> Courses
           </h1>
         </Link>
-        {window.location.pathname === '/Coursepage' && (
-          <div className="bg-slate-400"></div>
-        )}
+      
       </div>
 
       <div className="flex gap-2 mb-[1rem] items-center pl-3 hover.bg-[#f2b907]">
         <Link to="/Approved">
-          <h1 className="font-bold flex items-center justify-around gap-[0.3rem] cursor-pointer border-1 p-[0.4rem] text-[white]">
+          <h1 className= {`${location.pathname === "/Approved" && "bg-teal-300 p-10"}`}>
             <MdApproval className="h-[2rem] w-[1.5rem]" /> Approved
           </h1>
         </Link>
       </div>
       <div className="flex gap-2 mb-[1rem] items-center pl-3 hover:bg-[#f2b907]">
         <Link to="/Layout">
-          <h1 className="font-bold flex items-center justify-around gap-[0.3rem] cursor-pointer border-1 p-[0.4rem] text-[white]">
+          <h1 className= {`${location.pathname === "/Layout" && "bg-teal-300 p-10"}`}>
             <BiLogOut className="h-[2rem] w-[1.5rem]" />
             Log Out
           </h1>
