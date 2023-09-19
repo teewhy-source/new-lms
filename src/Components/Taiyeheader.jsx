e01cec464bedb92a5375d0ef76bfc70902c8b23b
teewhy-source <128053927+teewhy-source@users.noreply.github.com>
2023-09-19 11:54:52 +0100

    updtad

--- src/Components/Taiyeheader.jsx
+++ src/Components/Taiyeheader.jsx
@@ -13,7 +13,7 @@ export default class Taiyeheader extends Component {
         <nav className="container mx-auto px-4 py-2 flex flex-col md:flex-row justify-between items-center">
           <div className="text-white text-xl font-bold mb-4 md:mb-0 flex items-center">
             <img src={sail} alt="" className="h-[4rem] md:h-[5rem] rounded-full" />
-            <h1 className="text-[#08538c] ml-2">LMS</h1>
+            <h1 className="text-[#08538c] ml-2">SAILEARN</h1>
           </div>
           <div className="space-y-2 md:space-y-0 md:space-x-4">
             <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
